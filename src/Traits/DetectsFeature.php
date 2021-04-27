50f5d549066ff7884f49829ce577f97e1d78fb7c
glennmichael123 <gtorregosa@gmail.com>
2021-04-28 07:08:16 +0800

    Feature not found exception (#74)

--- src/Traits/DetectsFeature.php
+++ src/Traits/DetectsFeature.php
@@ -2,14 +2,27 @@
 
 namespace Meema\MeemaClient\Traits;
 
+use BadMethodCallException;
 use Meema\MeemaClient\Exceptions\FeatureNotImplementedException;
 
 trait DetectsFeature
 {
     public function __call($name, $arguments)
     {
-        if (! method_exists($this, $name)) {
+        $data = file_get_contents(dirname(dirname(__DIR__)).'/data/params.json');
+
+        $data = json_decode($data, true);
+
+        $params = array_keys($data['parameters']);
+
+        if (! method_exists($this, $name) && in_array($name, $params)) {
             throw new FeatureNotImplementedException('Feature not yet implemented. Please contact us if this is something you would like to see rather sooner than later.');
+        }
+
+        if (! method_exists($this, $name)) {
+            throw new BadMethodCallException(sprintf(
+                'Call to undefined method %s::%s()', static::class, $name
+            ));
         }
     }
 }
