5ca755c668adec96d2a8da5383e8f5608c00205a
Chris <chrisbreuer93@gmail.com>
2021-10-04 03:13:10 -0700

    Apply fixes from StyleCI (#112)

--- src/Exceptions/FeatureNotImplementedException.php
+++ src/Exceptions/FeatureNotImplementedException.php
@@ -9,7 +9,7 @@ class FeatureNotImplementedException extends Exception
     /**
      * Construct exception.
      *
-     * @param string $message
+     * @param  string  $message
      */
     public function __construct(string $message = null)
     {
